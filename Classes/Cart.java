9690a9089b59cc71c9dfd02ac914cd8df6441d27
Kareem Sabry <2301101@eng.asu.edu.eg>
2024-12-09 15:29:48 +0200

    edited Cart as array of products

--- Classes/Cart.java
+++ Classes/Cart.java
@@ -3,18 +3,41 @@ import java.util.ArrayList;
 
 public class Cart {
     //ArrayList is a dynamic array implemented by java :)
-    private ArrayList<Product> products;
+    private Product[] products = new Product[30];
+    private int count = 0;
     private double totalPrice;
 
     //Constructor
     public Cart() {
-        this.products =  new ArrayList<Product>();
-        this.totalPrice = 0;
+
     }
-    //Setters and Getter
-    //Methods
-    //addItem
-    //removeItem
-    //calculateTotal
-    //clearCart
+
+    public void addProduct(Product p) {
+        products[count++] = p;
+        this.updateTotal();
+    }
+    public void removeProduct(int index) {
+        if (index-- > count) { System.out.println("Invalid index"); return; }
+        products[index] = null;
+        for (int i = index; i < count; i++) {
+            products[i] = products[i + 1];
+        }
+    }
+    public void updateTotal(){
+
+        for (int i = 0; i < products.length; i++) {
+            totalPrice = 0;
+            totalPrice += products[i].getPrice();
+
+
+        }
+    }
+    void clearCart(){
+        products = new Product[30];
+        count = 0;
+        totalPrice = 0;
+    }
+
+
+
 }
